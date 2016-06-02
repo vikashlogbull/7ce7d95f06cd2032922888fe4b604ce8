63a3f7f1f34a72149d61141a80d870103b1f094e
stratosys <ashish@logbullit.com>
2016-06-02 12:14:46 +0000

    subsitevs0042.evolverinc.tech Subsite Deleted.

--- sites/sites.php
+++ sites/sites.php
@@ -60,3 +60,4 @@ $sites['subsitevs0038.evolverinc.tech'] = 'subsitevs0038.evolverinc.tech';
 $sites['subsitevs0039.evolverinc.tech'] = 'subsitevs0039.evolverinc.tech';
 $sites['subsitevs0040.evolverinc.tech'] = 'subsitevs0040.evolverinc.tech';
 $sites['subsitevs0041.evolverinc.tech'] = 'subsitevs0041.evolverinc.tech';
+$sites['subsitevs0042.evolverinc.tech'] = 'subsitevs0042.evolverinc.tech';
